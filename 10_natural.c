0fe08e055af964e8d538a2de77443f1323a9f079
sadasivuni sai dhanush <ssadasiv@student.gitam.edu>
2025-04-04 23:08:45 +0530

    Create 10_natural.c

---
+++ 10_natural.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+           int i, sum = 0;
+          // Calculate sum of 10 natural numbers
+          for (i = 1; i <= 10; i++)
+          {
+              sum += i;
+           }
+              printf("Sum of 10 natural numbers: %d\n", sum);
+              return 0;
+}
